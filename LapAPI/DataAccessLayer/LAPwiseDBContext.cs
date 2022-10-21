842da6d5a681634cbc9f814909861b3a1506759b
omkar <knomkarnag24@gmail.com>
2022-10-21 16:36:56 +0530

    Add project files.

---
+++ LapAPI/DataAccessLayer/LAPwiseDBContext.cs
@@ -0,0 +1,12 @@
+﻿using LapAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LapAPI.DataAccessLayer
+{
+    public class LAPwiseDBContext : DbContext
+    {
+        public LAPwiseDBContext(DbContextOptions<LAPwiseDBContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ LapAPI/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LapAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+    
+}
