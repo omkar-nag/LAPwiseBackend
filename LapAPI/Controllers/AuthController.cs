fc717069710991c4630b59116b38190bafedea81
Omkar Nag <knomkarnag24@gmail.com>
2022-10-22 11:23:33 +0530

    added config and token gen api

---
+++ LapAPI/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using LapAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LapAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthUserModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid User");
+            }
+
+            if (isValidUser(user))
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LAPwiseSecretKey@123"));
+
+                var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "http://localhost:5152",
+                    audience: "http://localhost:5152",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: signingCredentials
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+
+        private bool isValidUser(AuthUserModel user)
+        {
+            System.Diagnostics.Debug.WriteLine("\n\n");
+
+            System.Diagnostics.Debug.WriteLine(user.ToString());
+            System.Diagnostics.Debug.WriteLine("\n\n");
+
+            return true;
+        }
+    }
+}
--- LapAPI/Program.cs
+++ LapAPI/Program.cs
@@ -1,19 +1,56 @@
 using LapAPI.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<LAPwiseDBContext> (
-        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-    );
+// Adding JWT Authentication
 
+builder.Services.AddAuthentication(opt =>
+    {
+        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "http://localhost:5152",
+            ValidAudience = "http://localhost:5152",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LAPwiseSecretKey@123"))
+        };
+    });
+
+
+// CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("EnableCORS", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+
+builder.Services.AddDbContext<LAPwiseDBContext>(
+        options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
@@ -23,6 +60,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("EnableCORS");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
