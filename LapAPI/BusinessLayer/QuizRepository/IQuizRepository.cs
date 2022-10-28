628356f239b63ab0210f86a40187eec672326750
Omkar Nag <knomkarnag24@gmail.com>
2022-10-28 17:41:59 +0530

    assessments and quiz api

---
+++ LapAPI/BusinessLayer/QuizRepository/IQuizRepository.cs
@@ -0,0 +1,14 @@
+﻿using LapAPI.Models;
+
+namespace LapAPI.BusinessLayer.QuizRepository
+{
+    public interface IQuizRepository
+    {
+        Task<Quizzes?> GetById(int id);
+        //Task<Quizzes> Insert(Quizzes quiz);
+        //Task<Assessments> Update(Quizzes quiz);
+        Task<AssessmentResults> InsertAssessmentResult(AssessmentResults result);
+        //Task Delete(Quizzes quiz);
+        //Task Save();
+    }
+}
